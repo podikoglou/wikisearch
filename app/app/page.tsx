be91e90f0281a0d787cc3ef4daa33f952c670dee
Alex <alex.podikoglou@gmail.com>
2024-12-05 01:51:56 +0200

    refactor: layout

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -10,7 +10,7 @@ export default function () {
   const [results, setResults] = useState<QueryResult[]>([]);
 
   return (
-    <div className="flex flex-col max-w-sm mx-auto my-16 gap-4">
+    <div className="flex flex-col max-w-sm mx-auto my-16 gap-8">
       <h1 className="text-6xl font-bold text-center">Wikisearch</h1>
 
       <Search query={query} setResults={setResults} />
